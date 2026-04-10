cc7868a4be08426cd29b1e326c60eb67b8b48220
hampus_sahlen <hampus.sahlen@elev.ntig.se>
2026-04-10 11:57:07 +0200

    added separate file for db login

--- helpers/init.php
+++ helpers/init.php
@@ -8,8 +8,9 @@ if (PHP_VERSION_ID < 80100) { // php 8.1+ is required for page to run properly
 
 session_start(); // use session for login status
 
-require_once("util.php"); // contains utility functions
+require_once "util.php"; // contains utility functions
 
-require_once("database.php"); // contains database wrapper class
+require_once "database.php"; // contains database wrapper class
 
+require_once "database_credentials.php"; // contains db credentials, included in gitignore
 $dbObject = new DatabaseConnection($host, $user, $password, $database);
