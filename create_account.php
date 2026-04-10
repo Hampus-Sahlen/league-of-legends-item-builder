f11658bd5686d135336830fa46b6fe0c1d671866
hampus_sahlen <hampus.sahlen@elev.ntig.se>
2026-04-10 11:38:17 +0200

    change html to php

---
+++ create_account.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style/login.css">
+</head>
+<body>
+    <form>
+        <fieldset>
+            <legend></legend>
+            username <input type="text">
+            email <input type="text">
+            password <input type="text">
+            repeat password <input type="text">
+        </fieldset>
+    </form>
+</body>
+</html>
