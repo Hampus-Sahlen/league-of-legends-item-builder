bf0aaa9bd917b6893f06fc9e318c6c9578a0ba79
hampus_sahlen <hampus.sahlen@elev.ntig.se>
2026-04-10 11:23:59 +0200

    Added init file

---
+++ helpers/init.php
@@ -0,0 +1,15 @@
+<?php 
+if (PHP_VERSION_ID < 80100) { // php 8.1+ is required for page to run properly
+    exit("PHP 8.1+ required.");
+}
+
+// disable error reporting in prod
+// error_reporting(0); 
+
+session_start(); // use session for login status
+
+require_once("util.php"); // contains utility functions
+
+require_once("database.php"); // contains database wrapper class
+
+$dbObject = new DatabaseConnection($host, $user, $password, $database);
