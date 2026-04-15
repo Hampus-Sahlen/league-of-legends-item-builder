de5345b5fbbd5ce6fa8975c109d71d1151ed3319
Robin <robin.karlsson2@elev.ga.ntig.se>
2026-04-15 13:34:40 +0200

    tillagda bilder och ändrat strukturen på create_item.php och edit_item.php. Lagt till header, main, nav och footer element i båda filerna.

--- admin/create_item.php
+++ admin/create_item.php
@@ -7,9 +7,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Create Item</title>
 </head>
 <body>
-    
+    <header><h1>Create Item</h1></header>
+    <main></main>
+    <nav></nav>
+    <footer></footer>
 </body>
 </html>
