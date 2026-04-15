554e40a29e6766d1554e42ca6a6901aa457ba1ba
Hampus-Sahlen <hampus.sahlen@elev.ntig.se>
2026-04-15 13:24:22 +0200

    Update create_account.php
    added error place

--- create_account.php
+++ create_account.php
@@ -7,6 +7,9 @@
     <link rel="stylesheet" href="style/login.css">
 </head>
 <body>
+    <header>
+        error-ruta här
+    </header>
     <form>
         <fieldset>
             <legend></legend>
