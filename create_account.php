7d3d976c5ff60294bf66445f9ffb6cfa86c42a9d
hampus_sahlen <hampus.sahlen@elev.ntig.se>
2026-04-16 13:56:30 +0200

    account creation work in progress

--- create_account.php
+++ create_account.php
@@ -4,12 +4,18 @@ if (isset($_SESSION["UUID"]) && isset($_SESSION["accessLevel"])) {
     redirect("mainpage.php");
 }
 
-$errorMessage = "";
+$errorMessage = [];
 if (isset($_POST["username"])) { // try to create the user
     $username = $_POST["username"];
     $email = $_POST["email"];
     $password = $_POST["password"];
     $passwordRepeat = $_POST["password_repeat"];
+    
+    if (!preg_match("/^[\wåäöÅÄÖ]{3,30}$/", $username)){
+        $errorMessage[] = "Your username must have between 3 and 30 symbols. Allowed symbols: A to Z, ÅÄÖ and _";
+    }
+    // email regex: /^[^@\s]+@[^.\s]+\.[^\s]+\s*$/
+    // check duplicate emails before adding
 }
 
 ?>
@@ -29,10 +35,10 @@ if (isset($_POST["username"])) { // try to create the user
     <form>
         <fieldset>
             <legend></legend>
-            username <input type="text" id="username" name="username">
-            email <input type="text" id="email" name="email">
-            password <input type="text" id="password" name="password">
-            repeat password <input type="text" id="password_repeat" name="password_repeat">
+            username <input type="text" id="username" name="username" pattern="/^[\wåäöÅÄÖ]{3,30}$/" required>
+            email <input type="text" id="email" name="email" required>
+            password <input type="text" id="password" name="password" required>
+            repeat password <input type="text" id="password_repeat" name="password_repeat" required>
         </fieldset>
     </form>
 </body>
